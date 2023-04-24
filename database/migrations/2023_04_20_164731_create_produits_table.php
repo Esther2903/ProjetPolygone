5800f6bdbed35d844629307148c7e1d490b8b5b0
Raïssa Raïssath <raissaraissa376@gmail.com>
2023-04-24 08:55:45 +0100

    initialisation du projet

---
+++ database/migrations/2023_04_20_164731_create_produits_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produits', function (  $table) {
+            $table->id();
+            $table->string('libellé');
+            $table->double('Pu');
+            $table->int('QteStock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produits');
+    }
+};
